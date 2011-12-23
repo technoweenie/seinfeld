06b43c03d296f4b0a90b0f39b6c995c509615a6d
rick <technoweenie@gmail.com>
2011-12-22 23:56:23 -0700

    update from feed pages

--- lib/seinfeld/updater.rb
+++ lib/seinfeld/updater.rb
@@ -10,20 +10,22 @@ class Seinfeld
       @user = user
     end
 
-    def run(today = Date.today)
+    def run(today = Date.today, page = nil)
       today   ||= Date.today
       old_location = @user.location
       @user.update_location!
       @user.update_timezone! if old_location != @user.location
       Time.zone = @user.time_zone || "UTC"
-      if feed = Feed.fetch(@user)
-        @user.etag = feed.etag
-        @user.update_progress(feed.committed_days, today)
-      else
+      feed = Feed.fetch(@user, page)
+      if feed.disabled?
         @user.disabled = true
         @user.save!
+        nil
+      else
+        @user.etag = feed.etag
+        @user.update_progress(feed.committed_days, today)
+        feed
       end
-      feed
     end
   end
 end
